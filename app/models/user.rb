7a8593c5ac1ad41448c9c93d9318f17ac32e87a2
Kenny G. Wang <kennygwang@gmail.com>
2012-07-10 20:40:39 -0500

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
-  before_save :create_remember_token
+  has_many :microposts, :dependent => :destroy
   
+  before_save :create_remember_token
   before_save { |user| user.email = email.downcase }
   
   validates :name, :presence => true, :length => { :maximum => 50 }
@@ -23,6 +24,11 @@ class User < ActiveRecord::Base
                     :uniqueness => { :case_sensitive => false }
   validates :password, :presence => true, :length => { :minimum => 6 }
   validates :password_confirmation, :presence => true
+  
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
   
   private
 
